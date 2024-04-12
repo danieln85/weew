40d70107dd46593f32453f847e06064a410f05d0
Daniel Navas <danieln85@gmail.com>
2024-04-12 12:59:15 -0500

    fixed contact mail

--- app/Mail/ContactFormMail.php
+++ app/Mail/ContactFormMail.php
@@ -5,7 +5,6 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Address;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
@@ -14,42 +13,28 @@ class ContactFormMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $name;
+
     /**
      * Create a new message instance.
-     */
-    public function __construct(public $name)
-    {
-        //
-    }
-
-    /**
-     * Get the message envelope.
-     */
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Un usuario diligenció el Formulario de contacto!',
-            from: new Address(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'emails.contact-form',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
      *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     * @param string $name
      */
-    public function attachments(): array
+    public function __construct($name)
     {
-        return [];
+        $this->name = $name;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject('Un usuario diligenció el Formulario de contacto!')
+            ->from(config('mail.from.address'), config('mail.from.name'))
+            ->view('emails.contact-form');
     }
 }
