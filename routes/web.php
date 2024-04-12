af5d2085cbffaccf7186adc128fadf564c8c84f4
Daniel Navas <danieln85@gmail.com>
2024-04-12 18:28:07 -0500

    fixed ruta contact-us

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/nosotros', [AboutUsController::class, 'index'])->name('about-us');
 
+Route::get('/contactanos', [ContactController::class, 'index'])->name('contact-us');
 Route::resource('contact', ContactController::class);
 Route::post('store', [ContactController::class, 'store'])->name('store');
 
